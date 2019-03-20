1c941fe2a530b2cafbf5d838dba262a335a11d8a
dkrentzel <33486401+dkrentzel@users.noreply.github.com>
2019-03-20 18:22:03 +0000

    Update Lowpass_Filter.m

--- Lowpass_Filter.m
+++ Lowpass_Filter.m
@@ -1,6 +1,11 @@
 %Lowpass filter implementation for Third Year Project 9/3/19
+total_time = 26;
 
 lowpass_filter_signal = lowpass(acoustic_results(2,:), 0.01)
-time = acoustic_results(1, :);
+time = (total_time/15).*acoustic_results(1,:);
 
 plot(time, lowpass_filter_signal)
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Filtered Signal');
+axis([0 26 0 1.5]);
